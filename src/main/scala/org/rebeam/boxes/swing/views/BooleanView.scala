1089372c02075200a75d9f77b0dbceaeeb203cf0
trepidacious <trepidacious@gmail.com>
2015-10-08 14:11:08 +0100

    BooleanView uses BoxR and BoxM instead of Box

--- src/main/scala/org/rebeam/boxes/swing/views/BooleanView.scala
+++ src/main/scala/org/rebeam/boxes/swing/views/BooleanView.scala
@@ -29,17 +29,17 @@ case object Radio extends BooleanControlType
 case object Tab extends BooleanControlType
 
 object BooleanView {
-  def extended(v: Box[Boolean], n: Option[Box[String]], controlType: BooleanControlType = SlideCheck, icon: Option[Box[Icon]], toggle: Boolean = true) = new BooleanOptionView(v, n, new TConverter[Boolean], controlType, icon, toggle).asInstanceOf[SwingView]
-  def apply(v: Box[Boolean], controlType: BooleanControlType = SlideCheck, toggle: Boolean = true) = new BooleanOptionView(v, None, new TConverter[Boolean], controlType, None, toggle).asInstanceOf[SwingView]
-  def toolbar(v: Box[Boolean], icon: Option[Box[Icon]], toggle: Boolean = true) = new BooleanOptionView(v, None, new TConverter[Boolean], ToolbarButton, icon, toggle).asInstanceOf[SwingView]
+  def extended(v: BoxM[Boolean], n: BoxR[String] = just(""), controlType: BooleanControlType = SlideCheck, icon: BoxR[Option[Icon]], toggle: Boolean = true) = new BooleanOptionView(v, n, new TConverter[Boolean], controlType, icon, toggle).asInstanceOf[SwingView]
+  def apply(v: BoxM[Boolean], controlType: BooleanControlType = SlideCheck, toggle: Boolean = true) = new BooleanOptionView(v, just(""), new TConverter[Boolean], controlType, just(None), toggle).asInstanceOf[SwingView]
+  def toolbar(v: BoxM[Boolean], icon: BoxR[Option[Icon]], toggle: Boolean = true) = new BooleanOptionView(v, just(""), new TConverter[Boolean], ToolbarButton, icon, toggle).asInstanceOf[SwingView]
 }
 
 object BooleanOptionView {
-  def extended(v: Box[Option[Boolean]], n: Box[String], controlType: BooleanControlType = SlideCheck, icon: Option[Box[Icon]], toggle: Boolean = true) = new BooleanOptionView(v, Some(n), new OptionTConverter[Boolean], controlType, icon, toggle).asInstanceOf[SwingView]
-  def apply(v: Box[Option[Boolean]], controlType: BooleanControlType = SlideCheck, toggle: Boolean = true) = new BooleanOptionView(v, None, new OptionTConverter[Boolean], controlType, None, toggle).asInstanceOf[SwingView]
+  def extended(v: BoxM[Option[Boolean]], n: BoxR[String], controlType: BooleanControlType = SlideCheck, icon: BoxR[Option[Icon]], toggle: Boolean = true) = new BooleanOptionView(v, n, new OptionTConverter[Boolean], controlType, icon, toggle).asInstanceOf[SwingView]
+  def apply(v: BoxM[Option[Boolean]], controlType: BooleanControlType = SlideCheck, toggle: Boolean = true) = new BooleanOptionView(v, just(""), new OptionTConverter[Boolean], controlType, just(None), toggle).asInstanceOf[SwingView]
 }
 
-private class BooleanOptionView[G](v: Box[G], n: Option[Box[String]], c: GConverter[G, Boolean], controlType: BooleanControlType, icon: Option[Box[Icon]], toggle: Boolean = true) extends SwingView {
+private class BooleanOptionView[G](v: BoxM[G], n: BoxR[String], c: GConverter[G, Boolean], controlType: BooleanControlType, icon: BoxR[Option[Icon]], toggle: Boolean = true) extends SwingView {
 
   val component = controlType match {
     case Checkbox => new LinkingJCheckBox(this)
@@ -54,11 +54,16 @@ private class BooleanOptionView[G](v: Box[G], n: Option[Box[String]], c: GConver
 
   //Update delegate from Box
   val observer = {
+
+    val g: BoxScript[G] = for {
+      n <- v()
+    } yield n
+
     //TODO use applicative or similar to make this neater
     val script = for {
       newV <- v()
-      newN <- n.traverseU{_()}        //With an Option[Box[String]] and a Box[String]=>Script[String] we can get an Option[Script[String]], traverseU gives us a Script[Option[String]] instead, which is what we want
-      newIcon <- icon.traverseU{_()}
+      newN <- n
+      newIcon <- icon
     } yield (newV, newN, newIcon)  
 
     SwingView.observer(this, script){v => display(v._1, v._2, v._3)}
@@ -83,7 +88,7 @@ private class BooleanOptionView[G](v: Box[G], n: Option[Box[String]], c: GConver
   }
 
   //Update display if necessary
-  private def display(newV: G, newN: Option[String], newIcon: Option[Icon]) {
+  private def display(newV: G, newN: String, newIcon: Option[Icon]) {
     c.toOption(newV) match {
       case None => {
         model.enabled = false
@@ -95,9 +100,8 @@ private class BooleanOptionView[G](v: Box[G], n: Option[Box[String]], c: GConver
       }
     }
     model.fire
-    val newNValue = newN.getOrElse("")
-    if (newNValue != component.getText) {
-      component.setText(newNValue)
+    if (newN != component.getText) {
+      component.setText(newN)
     }
     val iconOrNull = newIcon.getOrElse(null)
     if (iconOrNull != component.getIcon) {
